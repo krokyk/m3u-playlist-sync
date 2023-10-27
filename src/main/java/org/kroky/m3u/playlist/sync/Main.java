3d7b3247235de5f93f15627c92a94c61e16e6c6a
Peter Krokavec <peter.krokavec@gmail.com>
2023-10-27 23:51:00 +0200

    added new option "-u" to directly replace playlist with the updated version that is in sync with dirs' content

--- src/main/java/org/kroky/m3u/playlist/sync/Main.java
+++ src/main/java/org/kroky/m3u/playlist/sync/Main.java
@@ -1,7 +1,9 @@
 package org.kroky.m3u.playlist.sync;
 
 import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,110 +21,143 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.FileUtils;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Options options = createOptions();
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd;
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            e.printStackTrace();
-            System.out.println();
-            HelpFormatter help = new HelpFormatter();
-            help.printHelp("m3u-playlist-sync", options);
-            return;
-        }
-        Set<String> dirNames = new TreeSet<>(Arrays.asList(cmd.getOptionValues("d")));
-        String playlistName = cmd.getOptionValue("p");
-        compare(dirNames, playlistName);
-    }
+	public static final Path OUTPUT_DIR = Paths.get(".").toAbsolutePath().normalize();
 
-    private static void compare(Set<String> dirPathNames, String playlistPathName) {
-        Set<String> musicFiles = new TreeSet<>(
-                dirPathNames.stream().map(dirName -> getFilesFromDir(Paths.get(dirName)).stream()).flatMap(s -> s)
-                        .map(path -> path.toString()).collect(Collectors.toList()));
-        try {
-            TreeSet<String> dirNames = new TreeSet<>(dirPathNames.stream()
-                    .map(dirPath -> Paths.get(dirPath).getFileName().toString()).collect(Collectors.toList()));
-            Path playlistPath = Paths.get(playlistPathName).toAbsolutePath().normalize();
-            Set<String> playlistEntries = new TreeSet<>(
-                    Files.lines(playlistPath).filter(line -> startsWith(line, dirNames)).collect(Collectors.toList()));
-            Path outputDir = Paths.get(".").toAbsolutePath().normalize();
+	public static void main(String[] args) throws Exception {
+		Options options = createOptions();
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd;
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.out.println();
+			HelpFormatter help = new HelpFormatter();
+			help.printHelp("m3u-playlist-sync", options);
+			return;
+		}
+		Set<String> dirPathNames = new TreeSet<>(Arrays.asList(cmd.getOptionValues("d")));
+		String playlistName = cmd.getOptionValue("p");
+		File playlistFile = new File(playlistName);
+		if (cmd.hasOption("u")) {
+			Path dirPlaylist = generatePlaylistFromDirs(dirPathNames);
+			if (playlistFile.isFile()) {
+				String backupPlaylistName = String.format("%s.%s", playlistFile.getName(), System.currentTimeMillis());
+				FileUtils.copyFile(playlistFile, dirPlaylist.getParent().resolve(backupPlaylistName).toFile());
+				FileUtils.delete(playlistFile);
+			}
+			FileUtils.copyFile(dirPlaylist.toFile(), playlistFile);
+			System.out.println(String.format("New playlist at: %s", playlistFile));
+		} else {
+			compare(dirPathNames, playlistFile);
+		}
+	}
 
-            Path filesOutput = outputDir.resolve("_fileEntries.txt").toAbsolutePath().normalize();
-            Files.write(filesOutput, musicFiles);
+	private static void compare(Set<String> dirPathNames, File playlistFile) throws Exception {
+		Path dirPlaylist = generatePlaylistFromDirs(dirPathNames);
 
-            Path playlistOutput = outputDir.resolve("_playlistEntries.txt").toAbsolutePath().normalize();
-            Files.write(playlistOutput, playlistEntries);
-            System.out.println("Results written to:");
-            System.out.println(filesOutput);
-            System.out.println(playlistOutput);
-            byte[] filesContents = Files.readAllBytes(filesOutput);
-            byte[] playlistContents = Files.readAllBytes(playlistOutput);
-            boolean equals = Arrays.equals(filesContents, playlistContents);
-            if (equals) {
-                System.out.println("You are SYNCED!");
-            } else {
-                System.out.println("The content is DIFFERENT!");
-                System.out.println("Playlist path: " + playlistPath);
-                Desktop.getDesktop().open(playlistPath.toFile());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		if (!playlistFile.isFile()) {
+			System.out.println(String.format("No such playlist exists: %s", playlistFile));
+			return;
+		}
 
-    }
+		TreeSet<String> dirNames = new TreeSet<>(dirPathNames.stream()
+				.map(dirPath -> Paths.get(dirPath).getFileName().toString()).collect(Collectors.toList()));
 
-    /**
-     *
-     * @param str
-     * @param strSet
-     * @return true if <code>str</code> starts with any of the strings contained in the <code>strSet</code>
-     */
-    private static boolean startsWith(String str, TreeSet<String> strSet) {
-        return strSet.stream().filter(dirName -> str.startsWith(dirName)).findFirst().isPresent();
-    }
+		Set<String> playlistEntries = new TreeSet<>(Files.lines(playlistFile.toPath())
+				.filter(line -> startsWith(line, dirNames)).collect(Collectors.toList()));
 
-    private static Set<Path> getFilesFromDir(Path dir) {
+		Path playlistOutput = OUTPUT_DIR.resolve("_playlistEntries.txt").toAbsolutePath().normalize();
+		writeBomAndLines(playlistOutput, playlistEntries);
+		System.out.println("Results written to:");
+		System.out.println(dirPlaylist);
+		System.out.println(playlistOutput);
+		byte[] filesContents = Files.readAllBytes(dirPlaylist);
+		byte[] playlistContents = Files.readAllBytes(playlistOutput);
+		boolean equals = Arrays.equals(filesContents, playlistContents);
+		if (equals) {
+			System.out.println("You are SYNCED!");
+		} else {
+			System.out.println("The content is DIFFERENT!");
+			System.out.println(String.format("Playlist path: %s", playlistFile));
+			Desktop.getDesktop().open(playlistFile);
+		}
+	}
 
-        try {
-            return Files.find(dir, Integer.MAX_VALUE, (p, a) -> isMusicFile(p, a))
-                    .map(absoluteFile -> relativeToDir(dir, absoluteFile, true)).collect(Collectors.toSet());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return Collections.emptySet();
-    }
+	private static Path generatePlaylistFromDirs(Set<String> dirPathNames) throws IOException {
+		Set<String> musicFiles = new TreeSet<>(
+				dirPathNames.stream().map(dirName -> getFilesFromDir(Paths.get(dirName)).stream()).flatMap(s -> s)
+						.map(path -> path.toString()).collect(Collectors.toList()));
 
-    private static Path relativeToDir(Path absoluteDir, Path absoluteFile, boolean includeParent) {
-        return includeParent ? absoluteDir.getParent().relativize(absoluteFile) : absoluteDir.relativize(absoluteFile);
-    }
+		Path filesOutput = OUTPUT_DIR.resolve("_fileEntries.txt").toAbsolutePath().normalize();
+		writeBomAndLines(filesOutput, musicFiles);
+		return filesOutput;
+	}
 
-    private static boolean isMusicFile(Path p, BasicFileAttributes a) {
-        String fileName = p.getFileName().toString().toLowerCase();
-        return a.isRegularFile() && (fileName.endsWith(".mp3") || fileName.endsWith(".flac"));
-    }
+	private static void writeBomAndLines(Path path, Set<String> lines) throws IOException {
+		File file = path.toFile();
+		FileUtils.write(file, "\uFEFF#EXTM3U\r\n", StandardCharsets.UTF_8); // write BOM
+		StringBuilder content = new StringBuilder();
+		lines.forEach(line -> content.append(line).append("\r\n"));
+		FileUtils.write(file, content, StandardCharsets.UTF_8, true);
+	}
 
-    private static Options createOptions() {
-        // create Options object
-        Options options = new Options();
+	/**
+	 *
+	 * @param str
+	 * @param strSet
+	 * @return true if <code>str</code> starts with any of the strings contained in
+	 */
+	private static boolean startsWith(String str, TreeSet<String> strSet) {
+		return strSet.stream().filter(dirName -> str.startsWith(dirName)).findFirst().isPresent();
+	}
 
-        Option d = Option.builder("d").longOpt("dirs").required().desc("one or more directories to read the files from") //
-                .hasArgs() // sets that number of arguments is unlimited
-                .build();
-        // add dirs option
-        options.addOption(d);
+	private static Set<Path> getFilesFromDir(Path dir) {
 
-        Option p = Option.builder("p").longOpt("playlist").required().desc("m3u playlist file to check") //
-                .numberOfArgs(1) // sets that number of arguments is unlimited
-                .build();
-        // add playlist option
-        options.addOption(p);
+		try {
+			return Files.find(dir, Integer.MAX_VALUE, (p, a) -> isMusicFile(p, a))
+					.map(absoluteFile -> relativeToDir(dir, absoluteFile, true)).collect(Collectors.toSet());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Collections.emptySet();
+	}
 
-        return options;
-    }
+	private static Path relativeToDir(Path absoluteDir, Path absoluteFile, boolean includeParent) {
+		return includeParent ? absoluteDir.getParent().relativize(absoluteFile) : absoluteDir.relativize(absoluteFile);
+	}
+
+	private static boolean isMusicFile(Path p, BasicFileAttributes a) {
+		String fileName = p.getFileName().toString().toLowerCase();
+		return a.isRegularFile() && (fileName.endsWith(".mp3") || fileName.endsWith(".flac"));
+	}
+
+	private static Options createOptions() {
+		// create Options object
+		Options options = new Options();
+
+		Option option = Option.builder("d").longOpt("dirs").required()
+				.desc("one or more directories to read the files from") //
+				.hasArgs() // sets that number of arguments is unlimited
+				.build();
+		// add dirs option
+		options.addOption(option);
+
+		option = Option.builder("p").longOpt("playlist").required().desc("m3u playlist file to check") //
+				.numberOfArgs(1).build();
+		// add playlist option
+		options.addOption(option);
+
+		option = Option.builder("u").longOpt("update").desc("updates the playlist") //
+				.numberOfArgs(0).build();
+		// add update option
+		options.addOption(option);
+
+		return options;
+	}
 
 }
