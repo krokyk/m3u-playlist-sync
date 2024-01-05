6572ef5aa7947f1bd79328fbb63521bed6cb0090
Peter Krokavec <peter.krokavec@gmail.com>
2024-01-05 18:02:57 +0100

    removed -u option, code formatted based on eclipse formatter

--- src/main/java/org/kroky/m3u/playlist/sync/Main.java
+++ src/main/java/org/kroky/m3u/playlist/sync/Main.java
@@ -1,6 +1,5 @@
 package org.kroky.m3u.playlist.sync;
 
-import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -12,7 +11,6 @@ import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
-
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -58,48 +56,15 @@ public class Main {
         Set<String> dirPathNames = new TreeSet<>(Arrays.asList(cmd.getOptionValues("d")));
         String playlistName = cmd.getOptionValue("p");
         File playlistFile = new File(playlistName);
-        if (cmd.hasOption("u")) {
-            Path dirPlaylist = generatePlaylistFromDirs(dirPathNames, playlistFile);
-            if (playlistFile.isFile()) {
-                String backupPlaylistName = String.format("%s.%s", playlistFile.getName(), System.currentTimeMillis());
-                FileUtils.copyFile(playlistFile, dirPlaylist.getParent().resolve(backupPlaylistName).toFile());
-                FileUtils.delete(playlistFile);
-            }
-            FileUtils.copyFile(dirPlaylist.toFile(), playlistFile);
-            System.out.println(String.format("New playlist at: %s", playlistFile.getAbsolutePath()));
-        } else {
-            compare(dirPathNames, playlistFile);
-        }
-    }
 
-    private static void compare(Set<String> dirPathNames, File playlistFile) throws Exception {
         Path dirPlaylist = generatePlaylistFromDirs(dirPathNames, playlistFile);
-
-        if (!playlistFile.isFile()) {
-            System.out.println(String.format("No such playlist exists: %s", playlistFile));
-            return;
+        if (playlistFile.isFile()) {
+            String backupPlaylistName = String.format("%s.%s", playlistFile.getName(), System.currentTimeMillis());
+            FileUtils.copyFile(playlistFile, dirPlaylist.getParent().resolve(backupPlaylistName).toFile());
+            FileUtils.delete(playlistFile);
         }
-
-        TreeSet<String> dirNames = new TreeSet<>(dirPathNames.stream().map(dirPath -> Paths.get(dirPath).getFileName().toString()).collect(Collectors.toList()));
-
-        Set<String> playlistEntries = new TreeSet<>(LEX_COMP);
-        playlistEntries.addAll(Files.lines(playlistFile.toPath()).filter(line -> startsWith(line, dirNames)).collect(Collectors.toList()));
-
-        Path playlistOutput = OUTPUT_DIR.resolve("_playlistEntries.txt").toAbsolutePath().normalize();
-        writeBomAndLines(playlistOutput, playlistEntries);
-        System.out.println("Results written to:");
-        System.out.println(dirPlaylist);
-        System.out.println(playlistOutput);
-        byte[] filesContents = Files.readAllBytes(dirPlaylist);
-        byte[] playlistContents = Files.readAllBytes(playlistOutput);
-        boolean equals = Arrays.equals(filesContents, playlistContents);
-        if (equals) {
-            System.out.println("You are SYNCED!");
-        } else {
-            System.out.println("The content is DIFFERENT!");
-            System.out.println(String.format("Playlist path: %s", playlistFile));
-            Desktop.getDesktop().open(playlistFile);
-        }
+        FileUtils.copyFile(dirPlaylist.toFile(), playlistFile);
+        System.out.println(String.format("New playlist at: %s", playlistFile.getAbsolutePath()));
     }
 
     private static Path generatePlaylistFromDirs(Set<String> dirPathNames, File playlistFile) throws IOException {
@@ -109,10 +74,10 @@ public class Main {
         for (String dirName : dirPathNames) {
             Path dir = Paths.get(dirName).toAbsolutePath(); // e:\Google Drive\Music\_vyber\BALLADS AND LOVE SONGS\LARA FABIAN - 1991 - Lara Fabian
             musicFiles.addAll(Files.walk(dir) //
-                    .filter(Main::isMusicFile) // get a full path to the music file
-                    .map(path -> playlistParent.relativize(path)) // relativize the music file's path to the playlist parent dir
-                    .map(Path::toString) // make String from it
-                    .collect(Collectors.toList())); // collect
+                .filter(Main::isMusicFile) // get a full path to the music file
+                .map(path -> playlistParent.relativize(path)) // relativize the music file's path to the playlist parent dir
+                .map(Path::toString) // make String from it
+                .collect(Collectors.toList())); // collect
         }
 
         Path filesOutput = OUTPUT_DIR.resolve("_fileEntries.txt").toAbsolutePath().normalize();
@@ -128,34 +93,20 @@ public class Main {
         FileUtils.write(file, content, StandardCharsets.UTF_8, true);
     }
 
-    /**
-     *
-     * @param str
-     * @param strSet
-     * @return true if <code>str</code> starts with any of the strings contained in
-     */
-    private static boolean startsWith(String str, TreeSet<String> strSet) {
-        return strSet.stream().filter(dirName -> str.startsWith(dirName)).findFirst().isPresent();
-    }
-
     private static Options createOptions() {
         // create Options object
         Options options = new Options();
 
-        Option option = Option.builder("d").longOpt("dirs").required().desc("one or more directories to read the files from") //
-                .hasArgs() // sets that number of arguments is unlimited
-                .build();
-        // add dirs option
+        Option option = Option.builder("d").longOpt("dirs").required() //
+            .desc("one or more directories to read the files from") //
+            .hasArgs() // sets that number of arguments is unlimited
+            .build();
         options.addOption(option);
 
-        option = Option.builder("p").longOpt("playlist").required().desc("m3u playlist file to check") //
-                .numberOfArgs(1).build();
-        // add playlist option
-        options.addOption(option);
-
-        option = Option.builder("u").longOpt("update").desc("updates the playlist") //
-                .numberOfArgs(0).build();
-        // add update option
+        option = Option.builder("p").longOpt("playlist").required() //
+            .desc("m3u playlist file to check") //
+            .numberOfArgs(1) // exactly 1 argument
+            .build();
         options.addOption(option);
 
         return options;
